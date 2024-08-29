e4fc794b44e8a0262657491cc4ab05ef8218dd60
safakb7 <safakb261@gmail.com>
2024-08-29 12:17:33 -0700

    spanish name updated

--- spanish_product_name/__manifest__.py
+++ spanish_product_name/__manifest__.py
@@ -7,17 +7,20 @@
                 ],
     'data': [
         'views/product_template_views.xml',
-        # 'views/pos_receipt_views.xml'
     ],
 
     'assets': {
         'point_of_sale._assets_pos': [
-            'spanish_product_name/static/src/js/product_product.js',
+            'spanish_product_name/static/src/js/product_order_line.js',
+             'spanish_product_name/static/src/xml/product_orderline_views.xml',
 
-            'spanish_product_name/static/src/js/spanish_product_name.js',
+            # 'spanish_product_name/static/src/js/product_product.js',
 
-            'spanish_product_name/static/src/xml/product_card_views.xml'
-            # 'spanish_product_name/static/src/xml/pos_receipt_views.xml',
+            # 'spanish_product_name/static/src/js/spanish_product_name.js',
+
+            # 'spanish_product_name/static/src/xml/product_card_views.xml'
+
+
 
         ],
     }
--- spanish_product_name/models/product_product.py
+++ spanish_product_name/models/product_product.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 
-class ProductTemplate(models.Model):
+class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     spanish_name = fields.Char(string='Spanish Name')
