e3a8376fd4ae09b7fba7fea0b9981dc8f77c7109
safa <safakb261@gmail.com>
2024-09-26 17:36:44 +0530

    pos edit

---
+++ pos_product_create/static/src/js/product_list.js
@@ -0,0 +1,15 @@
+/** @odoo-module */
+
+import { Component,useState } from "@odoo/owl";
+
+export class ProductListModal extends Component {
+    static template = "pos_create_list.ProductListModal";
+
+//    this.state = useState({
+//    products: [],
+//    });
+//    async show(products){
+//    this.state.products = products
+//    }
+
+}
--- pos_product_create/static/src/js/product_screen.js
+++ pos_product_create/static/src/js/product_screen.js
@@ -6,33 +6,13 @@ import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product
 
 export class CreateProduct extends Component {
     static template = "pos_create_button.CreateButton";
-    static props = {
-        class: { type: Object, optional: true },
-        line: {
-            type: Object,
-            shape: {
-                productName: String,
-                price: String,
-                qty: String,
 
-                unit: { type: String, optional: true },
-                unitPrice: String,
-
-                "*": true,
-            },
-        },
-    };
-    static defaultProps = {
-        class: {},
-
-    };
     setup() {
      this.pos = usePos()
     }
 
-     onClick() {
-        var product = this.pos.getProductInfo(product,1)
-        console.log(this)
+      onClick() {
+            var product = this.pos.get_product_list(); var list = new ProductListModal();
         }
-        }
+ }
 ProductScreen.addControlButton({ component: CreateProduct });
